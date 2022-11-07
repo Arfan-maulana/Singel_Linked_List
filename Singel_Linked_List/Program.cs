1d3cba9d055fc9564d28329db093c4d4d2ddb3e5
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-11-07 17:36:50 +0700

    menamhkan  list emepty

--- Singel_Linked_List/Program.cs
+++ Singel_Linked_List/Program.cs
@@ -130,7 +130,13 @@ namespace Singel_Linked_List
 
             }
         }
-        
+        public bool listEmpty()
+        {
+            if (START == null)
+                return true;
+            else
+                return false;
+        }
 
 
 
