659e412a8e037282cafb1b77a03f0d863862264d
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-11-07 16:48:20 +0700

    each node consist the information part and link to next node

--- Singel_Linked_List/Program.cs
+++ Singel_Linked_List/Program.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Singel_Linked_List
 {
-    internal class Program
+    //each node consist the information part and link to next node
+    class Node
+    {
+        public int RollNumber;
+        public string Name;
+        public Node Next;
+
+
+
+    }
+
+
+
+
+    class list
+    {
+
+
+
+    }
+     class Program
     {
         static void Main(string[] args)
         {
